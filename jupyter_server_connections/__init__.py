0d0edc3267cdef86a80766d95625ae3d6da027f9
Christopher Field <chris.field@theiascientific.com>
2025-12-13 13:16:34 -0500

    Add type annotations

--- jupyter_server_connections/__init__.py
+++ jupyter_server_connections/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
+from typing import Any, Dict, List
+
 from .app import ConnectionsExtensionApp
 
-def _jupyter_server_extension_points():
-    return [{"module": "connections.app", "app": ConnectionsExtensionApp}]
+
+def _jupyter_server_extension_points() -> List[Dict[str, Any]]:  # pragma: no cover
+    return [{"module": "jupyter_server_connections.app", "app": ConnectionsExtensionApp}]
