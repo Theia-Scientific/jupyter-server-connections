23c0d755d11a94474e4672aa48a207d9acc32507
Christopher Field <chris.field@theiascientific.com>
2026-06-16 15:35:00 -0400

    Fix lint errors

--- jupyter_server_connections/app.py
+++ jupyter_server_connections/app.py
@@ -5,6 +5,7 @@ from jupyter_server.services.kernels.handlers import _kernel_id_regex
 
 from .handlers import KernelConnectionHandler
 
+
 class ConnectionsExtensionApp(ExtensionApp):
     name = "jupyter_server_connections"
 
@@ -16,4 +17,5 @@ class ConnectionsExtensionApp(ExtensionApp):
     def initialize_settings(self):
         self.log.info("Initialize settings")
 
+ 
 main = launch_new_instance = ConnectionsExtensionApp.launch_instance
